47df8e831e268c60560f2224152f6414eb28727f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-24 22:11:20 +0700

    Allow custom anime id

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,31 @@ import MemoryCard from './components/MemoryCard.jsx'
 import './index.css'
 
 import AnilistApi from './anilistApi.js'
-const anilistApi = new AnilistApi()
+let anilistApi = new AnilistApi()
 
-async function fetchAndRender() {
+async function fetchAndRender(resetCallback) {
   await anilistApi.fetchData()
   anilistApi.logData()
   const characters = anilistApi.data.data.Media.characters.nodes
   root.render(
     <React.StrictMode>
       <MemoryCard characters={characters} />
+      <div className="w-full flex justify-center my-6 join">
+        <input
+          id="anilist-id"
+          type="text"
+          placeholder="Anilist anime id"
+          className="input input-bordered w-full max-w-xs join-item"
+        />
+        <button
+          className="btn btn-primary join-item"
+          onClick={() =>
+            resetCallback(document.getElementById('anilist-id').value)
+          }
+        >
+          Reset
+        </button>
+      </div>
     </React.StrictMode>
   )
 }
@@ -24,4 +40,9 @@ root.render(
   </React.StrictMode>
 )
 
-fetchAndRender()
+const resetCallback = (id) => {
+  anilistApi = new AnilistApi(id)
+  fetchAndRender(resetCallback)
+}
+
+fetchAndRender(resetCallback)
