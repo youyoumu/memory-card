28c7e5fab8018dd6ce6bac93f6744892fd0d6f45
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-24 14:39:31 +0700

    Remove default project files

---
+++ src/MemoryCard.jsx
@@ -0,0 +1,8 @@
+export default function MemoryCard() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button className="btn">Button</button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import MemoryCard from './MemoryCard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MemoryCard />
+  </React.StrictMode>
 )
